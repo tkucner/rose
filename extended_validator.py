b1e3fa8333a825629f013413c2330fb7ed925a51
Tomasz Kucner <tomasz.kucner@oru.se>
2021-03-06 16:27:49 +0100

    completed update from develop

---
+++ extended_validator.py
@@ -0,0 +1,44 @@
+import json
+
+from jsonschema import Draft7Validator, validators, exceptions
+
+
+class ExtendedValidator:
+    def __init__(self, jason_path, schema_path):
+        self.my_schema = self.__load_json(schema_path)
+        self.my_json = self.__load_json(jason_path)
+
+    def extended_validator(self):
+        DefaultValidatingDraft7Validator = self.__extend_with_default(Draft7Validator)
+
+        try:
+            DefaultValidatingDraft7Validator(self.my_schema).validate(self.my_json)
+        except exceptions.ValidationError as e:
+            return False, e
+        except exceptions.SchemaError as e:
+            return False, e
+        return True, self.my_json
+
+    @staticmethod
+    def __extend_with_default(validator_class):
+        validate_properties = validator_class.VALIDATORS["properties"]
+
+        def set_defaults(validator, properties, instance, schema):
+            for property, subschema in properties.items():
+                if "default" in subschema:
+                    instance.setdefault(property, subschema["default"])
+
+            for error in validate_properties(
+                    validator, properties, instance, schema,
+            ):
+                yield error
+
+        return validators.extend(
+            validator_class, {"properties": set_defaults},
+        )
+
+    @staticmethod
+    def __load_json(path):
+        with open(path, 'r') as f:
+            my_json_file = json.load(f)
+            return my_json_file
---
+++ wall_segment.py
@@ -0,0 +1,26 @@
+import numpy as np
+from shapely.geometry import MultiPoint
+
+
+class WallSegment(MultiPoint):
+    def __init__(self):
+        self.id = -1
+        self.cells = None
+
+    @staticmethod
+    def compute_corner(a, corner):
+        return [a[0] + corner[0], a[1] + corner[1]]
+
+    def add_cells(self, cells):
+        corners = np.array([[0.5, 0.5],
+                            [-0.5, 0.5],
+                            [-0.5, -0.5],
+                            [0.5, -0.5]])
+        if self.cells is None:
+            self.cells = cells
+
+            temp_cell_corners = np.empty((0, 2), float)
+            for corner in corners:
+                new = np.apply_along_axis(WallSegment.compute_corner, 1, self.cells, corner=corner)
+                temp_cell_corners = np.append(temp_cell_corners, np.array(new), axis=0)
+            super().__init__(temp_cell_corners)
